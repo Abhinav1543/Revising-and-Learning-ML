2ee8ee5812cf2e31b1b2b2eda29fa5665a9fae52
Abhinav Sharma <abhii9780244826@gmail.com>
2026-05-01 23:11:31 +0530

    revision data frames

--- panda_basics/test_panda.py
+++ panda_basics/test_panda.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
 data = {
-    "Name" : ["Abhinav","Arya","Aryan","Ashwani","Akasht"],
-    "Age" : [1,3,42,51,66]
+    "Name" : ["Abhinav","Arya","Aryan","Ashwani","Akasht","Anmol","Sarika","Deep"],
+    "Age" : [1,3,42,51,66,100,1000,11],
+    "Marks" : [22,33,44,55,66,100,10,1] 
 }
 
 df = pd.DataFrame(data)
-print(df)
+# print(df)
+
+# print(df.head())
+# print(df.tail())
+print(df.describe())
+print(df.info())
+
